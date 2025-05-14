a6a0248050b99e53eec58cc6b0c322769dcce2b9
brandonthompson3 <123602623+brandonthompson3@users.noreply.github.com>
2025-05-14 00:26:15 -0700

    Updated Key Pickup

--- Source/GauntletClone/Private/KeyPickup.cpp
+++ Source/GauntletClone/Private/KeyPickup.cpp
@@ -1,52 +1,41 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+//Copyright Epic Games, Inc. All Rights Reserved.
+//Brandon
 
 #include "KeyPickup.h"
 #include "Kismet/GameplayStatics.h"
-// #Include player character header here
-// #include "GauntletCharacter.h"
+//#Include player character header here
+//#include "GauntletCharacter.h"
 
 AKeyPickup::AKeyPickup()
 {
-    // Set default values
+    //Set default values
     KeyID = 1;
     bSingleUse = true;
-    KeyColor = FLinearColor::Yellow; // Default key color
 
-    // Set default mesh for key
+    //Set default mesh for key
     static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("/Engine/BasicShapes/Cone.Cone"));
     if (MeshAsset.Succeeded())
     {
         PickupMesh->SetStaticMesh(MeshAsset.Object);
-        PickupMesh->SetRelativeScale3D(FVector(0.3f, 0.3f, 0.8f)); // Make it look key-shaped
-        PickupMesh->SetRelativeRotation(FRotator(90.0f, 0.0f, 0.0f)); // Rotate to stand upright
+        PickupMesh->SetRelativeScale3D(FVector(0.3f, 0.3f, 0.8f));
+        PickupMesh->SetRelativeRotation(FRotator(90.0f, 0.0f, 0.0f));
     }
-
-    // You would want to set a proper key mesh in your game
-    // PickupMesh->SetStaticMesh(YourKeyMesh);
-
-    // Keys are important pickups
-    PickupPriority = 2; // Higher priority for UI indicators
+    PickupPriority = 2; //Higher priority for UI indicators
 }
 
 void AKeyPickup::OnPickedUp(AActor* PickupActor)
 {
-    // Call parent implementation
+    //Call parent implementation
     Super::OnPickedUp(PickupActor);
 
-    // Check if the actor is a character (check for your specific player class)
+    UE_LOG(LogTemp, Display, TEXT("Key pickup collected: KeyID=%d, SingleUse=%s"),
+        KeyID, bSingleUse ? TEXT("true") : TEXT("false"));
+
+    //Check for your specific player class
     // APlayerCharacter* PlayerCharacter = Cast<APlayerCharacter>(PickupActor);
     // if (PlayerCharacter)
     // {
     //     // Add key to player's inventory
     //     PlayerCharacter->AddKey(KeyID, bSingleUse);
     // }
-
-    // For now, we'll just log that a key was collected
-    UE_LOG(LogTemp, Display, TEXT("Key pickup collected: KeyID=%d, SingleUse=%s"),
-        KeyID, bSingleUse ? TEXT("true") : TEXT("false"));
-
-    // Potentially broadcast an event that doors can listen to
-    // This is an alternative to having the player store keys
-    // FString EventName = FString::Printf(TEXT("KeyCollected_%d"), KeyID);
-    // UGameplayStatics::BroadcastDynamicEvent(GetWorld(), FName(*EventName), GetInstigator());
 }
--- Source/GauntletClone/Public/KeyPickup.h
+++ Source/GauntletClone/Public/KeyPickup.h
@@ -1,4 +1,5 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+//Copyright Epic Games, Inc. All Rights Reserved.
+//Brandon
 
 #pragma once
 
@@ -15,23 +16,18 @@ class GAUNTLETCLONE_API AKeyPickup : public ABasePickup
     GENERATED_BODY()
 
 public:
-    // Sets default values for this pickup's properties
+    //Sets default values for this pickup's properties
     AKeyPickup();
 
 protected:
-    // Override the OnPickedUp function to implement key collection
+    //Override the OnPickedUp function to implement key collection
     virtual void OnPickedUp(AActor* PickupActor) override;
 
-    // Key ID to identify which door(s) this key can open
-    // Use 0 for a master key that opens all doors
+    //Key ID to identify which door(s) this key can open (Use 0 for a master key that opens all doors)
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup|Key")
     int32 KeyID;
 
-    // Whether this key disappears after a single use
+    //Whether this key disappears after a single use
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup|Key")
     bool bSingleUse;
-
-    // Optional color to visually distinguish different key types
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup|Key")
-    FLinearColor KeyColor;
 };
