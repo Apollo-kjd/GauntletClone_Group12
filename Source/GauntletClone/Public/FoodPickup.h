17a5a3f90f433eeda4a20e97fa1bb93dcd888774
brandonthompson3 <123602623+brandonthompson3@users.noreply.github.com>
2025-05-14 00:12:00 -0700

    Updated Food Pickup

--- Source/GauntletClone/Public/FoodPickup.h
+++ Source/GauntletClone/Public/FoodPickup.h
@@ -1,4 +1,5 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
+//Brandon
 
 #pragma once
 
@@ -15,14 +16,14 @@ class GAUNTLETCLONE_API AFoodPickup : public ABasePickup
     GENERATED_BODY()
 
 public:
-    // Sets default values for this pickup's properties
+    //Sets default values for this pickup's properties
     AFoodPickup();
 
 protected:
-    // Override the OnPickedUp function to implement health restoration
+    //Override the OnPickedUp function to implement health restoration
     virtual void OnPickedUp(AActor* PickupActor) override;
 
-    // Amount of health this food item restores
+    //Amount of health this food item restores
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup|Food")
     float HealthRestoreAmount;
 };
